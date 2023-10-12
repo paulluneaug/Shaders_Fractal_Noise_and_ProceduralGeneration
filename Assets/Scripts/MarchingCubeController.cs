33e6515dfe2ec4406de1f319c81748f59ada0c43
p.luneau <p.luneau@acreos.eu>
2023-10-12 16:56:09 +0200

    Added regions and comments + Misc for simplifier

--- Assets/Scripts/MarchingCubeController.cs
+++ Assets/Scripts/MarchingCubeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -52,12 +53,13 @@ public class MarchingCubeController : MonoBehaviour
     #endregion
 
 
-
+    #region Shader Properties Names
+    // MarchingCube CS
     private const string NOISE_KERNEL_NAME = "ComputeNoise";
     private const string MARCHING_CUBE_KERNEL_NAME = "MarchingCubes";
 
     private const string NOISE_TEXTURE = "_NoiseTexture";
-    private const string RESULT_MESHES = "_ResultMeshes";
+    private const string GENERATED_MESHES = "_GeneratedMeshes";
 
     private const string CHUNK_ZONE_TO_GENERATE_SIZE = "_ChunkZoneSizeToGenerate";
     private const string CHUNK_OFFSET = "_ChunkOffset";
@@ -68,12 +70,23 @@ public class MarchingCubeController : MonoBehaviour
     private const string NOISE_LAYERS = "_NoiseLayers";
     private const string NOISE_WEIGHTS_MULTIPLIER = "_NoiseWeigthsMultiplier";
 
+    // MeshSimplifier CS
+    private const string CHUNKIFY_MESHES_KERNEL_NAME = "ChunkifyMeshes";
+    private const string SIMPLIFY_CHUNKS_KERNEL_NAME = "SimplifyChunks";
+
+    private const string REORGANIZED_MESHES_INDEX_MAP = "_ReorganizedMeshesIndexMap";
+    private const string RESULT_CHUNKS = "_ResultChunks";
+    #endregion
+
+    #region Properties
     private Vector3Int CellOffset => m_chunkOffset * CHUNK_SIZE;
 
     private Vector3Int CellsToGenerateSize => m_chunkZoneSizeToGenerate * CHUNK_SIZE;
+    #endregion
 
-
+    #region Serialized Fields
     [SerializeField] private ComputeShader m_marchingCubeCS = null;
+    [SerializeField] private ComputeShader m_meshSimplifierCS = null;
 
     [SerializeField] private Vector3Int m_chunkOffset = Vector3Int.zero;
     [SerializeField] private Vector3Int m_chunkZoneSizeToGenerate = Vector3Int.one * 4;
@@ -83,13 +96,16 @@ public class MarchingCubeController : MonoBehaviour
     [SerializeField] private NoiseLayer3D[] m_noiseLayers = null;
 
     [SerializeField] private Material m_material = null;
+    #endregion
 
     // Cache
+    #region Shader Properties IDs
+    // MarchingCube CS
     [NonSerialized] private int m_noiseKernelID = 0;
     [NonSerialized] private int m_marchingCubeKernelID = 0;
 
     [NonSerialized] private int m_noiseTexturePropertyID = 0;
-    [NonSerialized] private int m_resultMeshesPropertyID = 0;
+    [NonSerialized] private int m_generatedMeshesPropertyID = 0;
 
     [NonSerialized] private int m_chunkZoneSizeToGeneratePropertyID = 0;
     [NonSerialized] private int m_chunkOffsetPropertyID = 0;
@@ -100,21 +116,42 @@ public class MarchingCubeController : MonoBehaviour
     [NonSerialized] private int m_noiseLayersPropertyID = 0;
     [NonSerialized] private int m_noiseWeightsMultiplierPropertyID = 0;
 
+    // MeshSimplifier CS
+    [NonSerialized] private int m_chunkifyMeshesKernelID = 0;
+    [NonSerialized] private int m_simplifyChunksKernelID = 0;
+
+    [NonSerialized] private int m_reorganizedMeshesIndexMapPropertyID = 0;
+    [NonSerialized] private int m_resultChunksPropertyID = 0;
+    #endregion
+
     [NonSerialized] private ScriptExecutionTimeRecorder m_recorder = null;
 
     [NonSerialized] private RenderTexture m_noiseTexture = null;
-    [NonSerialized] private ComputeBuffer m_resultMeshesBuffer = null;
     [NonSerialized] private ComputeBuffer m_noiseLayersBuffer = null;
+    [NonSerialized] private ComputeBuffer m_generatedMeshesBuffer = null;
+    [NonSerialized] private ComputeBuffer m_reorganizedMeshesIndexMapBuffer = null;
 
     [NonSerialized] private Transform m_transform = null;
 
     private void Awake()
     {
+        GetPropertiesIDs();
+
+        m_recorder = new ScriptExecutionTimeRecorder();
+
+        m_transform = transform;
+
+        UpdateShaderProperty();
+    }
+
+    private void GetPropertiesIDs()
+    {
+        // MarchingCube CS
         m_noiseKernelID = m_marchingCubeCS.FindKernel(NOISE_KERNEL_NAME);
         m_marchingCubeKernelID = m_marchingCubeCS.FindKernel(MARCHING_CUBE_KERNEL_NAME);
 
         m_noiseTexturePropertyID = Shader.PropertyToID(NOISE_TEXTURE);
-        m_resultMeshesPropertyID = Shader.PropertyToID(RESULT_MESHES);
+        m_generatedMeshesPropertyID = Shader.PropertyToID(GENERATED_MESHES);
 
         m_chunkZoneSizeToGeneratePropertyID = Shader.PropertyToID(CHUNK_ZONE_TO_GENERATE_SIZE);
         m_chunkOffsetPropertyID = Shader.PropertyToID(CHUNK_OFFSET);
@@ -125,18 +162,20 @@ public class MarchingCubeController : MonoBehaviour
         m_noiseLayerCountPropertyID = Shader.PropertyToID(NOISE_LAYERS_COUNT);
         m_noiseWeightsMultiplierPropertyID = Shader.PropertyToID(NOISE_WEIGHTS_MULTIPLIER);
 
-        m_recorder = new ScriptExecutionTimeRecorder();
+        // MeshSimplifier CS
+        m_chunkifyMeshesKernelID = m_meshSimplifierCS.FindKernel(CHUNKIFY_MESHES_KERNEL_NAME);
+        m_simplifyChunksKernelID = m_meshSimplifierCS.FindKernel(SIMPLIFY_CHUNKS_KERNEL_NAME);
 
-        m_transform = transform;
-
-        UpdateShaderProperty();
+        m_reorganizedMeshesIndexMapPropertyID = Shader.PropertyToID(REORGANIZED_MESHES_INDEX_MAP);
+        m_resultChunksPropertyID = Shader.PropertyToID(RESULT_CHUNKS);
     }
 
     private void UpdateShaderProperty()
     {
         m_recorder.Reset();
 
-        SetShaderProperties();
+        SetMarchingCubeShaderProperties();
+        SetMeshSimplifierShaderProperties();
 
         m_recorder.AddEvent("Shader properties assignation");
 
@@ -155,17 +194,25 @@ public class MarchingCubeController : MonoBehaviour
         m_recorder.AddEvent("Marching cubes Shader Dispatch");
 
         CubeMesh[] result = new CubeMesh[CellsToGenerateSize.x * CellsToGenerateSize.y * CellsToGenerateSize.z];
-        m_resultMeshesBuffer.GetData(result);
+        m_generatedMeshesBuffer.GetData(result);
         m_recorder.AddEvent("Data acquisition from shader");
 
         GenerateMeshes(result);
         m_recorder.AddEvent("Meshes Generation");
 
-
         m_recorder.LogAllEventsTimeSpan();
     }
 
-    private void SetShaderProperties()
+    private void SetMeshSimplifierShaderProperties()
+    {
+        m_meshSimplifierCS.SetBuffer(m_chunkifyMeshesKernelID, m_generatedMeshesPropertyID, m_generatedMeshesBuffer);
+
+        m_reorganizedMeshesIndexMapBuffer?.Release();
+        m_reorganizedMeshesIndexMapBuffer = new ComputeBuffer(CellsToGenerateSize.x * CellsToGenerateSize.y * CellsToGenerateSize.z, sizeof(int));
+
+    }
+
+    private void SetMarchingCubeShaderProperties()
     {
         GPUNoiseLayer3D[] gpuNoiseLayers = m_noiseLayers
             .Where(layer => layer.Enabled)
@@ -175,9 +222,9 @@ public class MarchingCubeController : MonoBehaviour
         float weightMultiplier = 1.0f / gpuNoiseLayers.Select(layer => layer.LayerWeigth).Sum();
 
         // Meshes Buffer
-        m_resultMeshesBuffer?.Release();
-        m_resultMeshesBuffer = new ComputeBuffer(CellsToGenerateSize.x * CellsToGenerateSize.y * CellsToGenerateSize.z, Marshal.SizeOf(typeof(CubeMesh)));
-        m_marchingCubeCS.SetBuffer(m_marchingCubeKernelID, m_resultMeshesPropertyID, m_resultMeshesBuffer);
+        m_generatedMeshesBuffer?.Release();
+        m_generatedMeshesBuffer = new ComputeBuffer(CellsToGenerateSize.x * CellsToGenerateSize.y * CellsToGenerateSize.z, Marshal.SizeOf(typeof(CubeMesh)));
+        m_marchingCubeCS.SetBuffer(m_marchingCubeKernelID, m_generatedMeshesPropertyID, m_generatedMeshesBuffer);
 
         // Other variables
         m_marchingCubeCS.SetFloat(m_thresholdID, m_threshold);
@@ -204,6 +251,7 @@ public class MarchingCubeController : MonoBehaviour
         m_marchingCubeCS.SetFloat(m_noiseWeightsMultiplierPropertyID, weightMultiplier);
     }
 
+    #region Mesh Creation
     private void GenerateMeshes(CubeMesh[] meshes)
     {
         for (int i = 0; i < meshes.Length; i++)
@@ -244,4 +292,5 @@ public class MarchingCubeController : MonoBehaviour
         int x = indexY % CellsToGenerateSize.x;
         return new Vector3Int(x, y, z);
     }
+    #endregion
 }
